b45683d8bc69a288bc22d3854d668feba88b920d
wilian santos <14809032647@estudante.ufjf.br>
2024-12-02 19:58:34 -0300

    Adiciona animal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
-import './App.css'
+import Animal from './Animal';
+import './App.css';
 
 function App() {
 
-  return <div className="app">Ola mundo</div>;
+  return <div className="app">
+      <Animal/> 
+  
+  </div>;
 }
 
 export default App
