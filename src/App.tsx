97a26dad22c180bcbd002201af2a4b95a2659dd4
wilian santos <14809032647@estudante.ufjf.br>
2024-12-09 19:25:18 -0300

    extrai vetores

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import { ReactNode } from 'react';
 import Animal from './Animal';
 import './App.css';
 import Exibicao from './Exibicao';
 
 function App() {
 
+  const exA1: ReactNode[]=[
+  
+    <Animal icone="🐻" nome="Urso" peso = {-1} exticao/>, 
+    <Animal icone="🐕‍🦺" nome="Cachorro" peso = {50} exticao/> 
+  
+  ];
+  
+  const exB2: ReactNode[] =[
+    
+    <Animal icone="🦍" nome="Gorila" peso = {200} exticao/>, 
+    <Animal icone="🦥" nome="Preguica" peso = {100} exticao/> 
+
+
+  ];
+
   return <div className="app">
      <Exibicao 
       abertura={new Date("2024-11-06T08:00-03:00")}
       fechamento={new Date("2024-11-06T12:00-03:00")} cercado="A1"
-      >
+      > {exA1}
       
-        <Animal icone="🐻" nome="Urso" peso = {-1} exticao/> 
-        <Animal icone="🐕‍🦺" nome="Cachorro" peso = {50} exticao/> 
+      
       
       </Exibicao>
       <Exibicao
@@ -19,9 +34,8 @@ function App() {
           abertura={new Date("2024-11-06T13:00-03:00")}
           fechamento={new Date("2024-11-06T17:00-03:00")} 
           cercado="B1"
-      >
+      >{exB2}
 
-      <Animal icone="🦍" nome="Gorila" peso = {200} exticao/> 
   
   </Exibicao>
   </div>;
